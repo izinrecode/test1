7faa1ab0270ae9bfa9821ce90149df0eb6ab0c56
izinrecode <83387876+izinrecode@users.noreply.github.com>
2021-08-06 13:41:54 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -47,7 +47,7 @@ clear
 echo " "
 echo "Installation has been completed!!"
 echo " "
-echo "=================================-Script Mod SL YNTKTS-===========================" | tee -a log-install.txt
+echo "=================================-Script JOMBLOSSH-===========================" | tee -a log-install.txt
 echo "" | tee -a log-install.txt
 echo "----------------------------------------------------------------------------------" | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
@@ -84,7 +84,7 @@ echo "   - IPv6                    : [OFF]"  | tee -a log-install.txt
 echo "   - Autoreboot On 00.00 GMT +7" | tee -a log-install.txt
 echo "   - Installation Log --> /root/log-install.txt"  | tee -a log-install.txt
 echo ""  | tee -a log-install.txt
-echo "------------------------------------------Mod by SL------------------------------------------" | tee -a log-install.txt
+echo "------------------------------------------JSTFREENET------------------------------------------" | tee -a log-install.txt
 echo ""
 echo " Reboot 10 Sec"
 sleep 10
