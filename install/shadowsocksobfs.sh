9a0507f282a531c0a908ac0858c0919b63fff7ef
izinrecode <83387876+izinrecode@users.noreply.github.com>
2021-08-06 13:58:42 +0700

    Update shadowsocksobfs.sh

--- install/shadowsocksobfs.sh
+++ install/shadowsocksobfs.sh
@@ -5,7 +5,7 @@ OS=$ID
 ver=$VERSION_ID
 
 #
-GitUser="fisabiliyusri"
+GitUser="izinrecode"
 #wget https://github.com/${GitUser}/
 #Install_Packages
 echo "#############################################"
