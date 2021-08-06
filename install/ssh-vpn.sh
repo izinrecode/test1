8c75e1a6e13e32713dacfa864453d9eb1c71d1c5
izinrecode <83387876+izinrecode@users.noreply.github.com>
2021-08-06 14:44:47 +0700

    Create ssh-vpn.sh

--- install/ssh-vpn.sh
+++ install/ssh-vpn.sh
@@ -88,7 +88,7 @@ sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 apt-get --reinstall --fix-missing install -y bzip2 gzip coreutils wget screen rsyslog iftop htop net-tools zip unzip wget net-tools curl nano sed screen gnupg gnupg1 bc apt-transport-https build-essential dirmngr libxml-parser-perl neofetch git
 echo "clear" >> .profile
 echo "neofetch" >> .profile
-echo "echo Mod by SL" >> .profile
+echo "echo JOMBLOSSH" >> .profile
 
 # install webserver
 apt -y install nginx
